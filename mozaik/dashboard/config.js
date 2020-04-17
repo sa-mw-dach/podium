afe5c1958cea1159f9bba66a6212d209fe1bfd6d
Sebastian Hetze <shetze@redhat.com>
2020-04-17 18:06:46 +0200

    change domain name

--- mozaik/dashboard/config.js
+++ mozaik/dashboard/config.js
@@ -38,13 +38,13 @@ var config = {
             widgets: [
                 {
                     type: 'github.user_badge',
-                    user: 'shetze',
+                    user: 'sa-mw-dach',
                     columns: 1, rows: 1,
                     x: 0, y: 0
                 },
                 {
                     type: 'github.repository_contributors_stats',
-                    repository: 'shetze/podium-dashboard',
+                    repository: 'sa-mw-dach/podium',
                     columns: 1, rows: 1,
                     x: 2, y: 0
                 },
@@ -56,7 +56,7 @@ var config = {
                 {
                     type: 'embed.markup',
                     title: 'Conference Center',
-                    content: '<center><a href="https://meet-jitsi.apps.cloud.lunetix.org/Plenum" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2013/02/20/01/04/meeting-83519__340.jpg" alt="Main Podium" width="300" height="200"></a>  <a href="https://meet-jitsi.apps.cloud.lunetix.org/RoomA" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2015/07/02/09/52/interior-design-828545__340.jpg" alt="Meeting Room" width="300" height="200"></a>  <a href="https://meet-jitsi.apps.cloud.lunetix.org/Lobby" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2019/11/29/08/34/space-4660847__340.jpg" alt="Lobby" width="300" height="200"></a></center>',
+                    content: '<center><a href="https://meet-jitsi.apps.cloud.example.com/Plenum" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2013/02/20/01/04/meeting-83519__340.jpg" alt="Main Podium" width="300" height="200"></a>  <a href="https://meet-jitsi.apps.cloud.example.com/RoomA" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2015/07/02/09/52/interior-design-828545__340.jpg" alt="Meeting Room" width="300" height="200"></a>  <a href="https://meet-jitsi.apps.cloud.example.com/Lobby" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2019/11/29/08/34/space-4660847__340.jpg" alt="Lobby" width="300" height="200"></a></center>',
                     columns: 2, rows: 1,
                     x: 1, y: 1
                 },
@@ -66,28 +66,30 @@ var config = {
                     x: 3, y: 0
                 },
                 {
-                    type: 'mozaik.inspector',
+                    type: 'embed.markup',
+                    title: 'Kanban Board',
+                    content: '<center><a href="https://wekan-jitsi.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener"><img src="https://wekan.github.io/wekan-logo.svg" alt="Podium Kanban" width="300" height="200"></a></center>',
                     columns: 1, rows: 1,
                     x: 0, y: 1
                 },
                 {
                     type: 'embed.markup',
                     title: 'Sketch Boards',
-                    content: '<ul><li><a href="https://etherpad-jitsi.apps.cloud.lunetix.org/" target="_tab" rel="noreferrer noopener">New Pad</a></li> <li><a href="https://etherpad-jitsi.apps.cloud.lunetix.org/p/Podium" target="_tab" rel="noreferrer noopener">General Podium Pad</a></li> <li><a href="https://etherpad-jitsi.apps.cloud.lunetix.org/p/Draft" target="_tab" rel="noreferrer noopener">Draft Pad</a></li> <li><a href="https://etherpad-jitsi.apps.cloud.lunetix.org/p/ToDo" target="_tab" rel="noreferrer noopener">ToDo Pad</a></li></ul>',
+                    content: '<ul><li><a href="https://etherpad-jitsi.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener">New Pad</a></li> <li><a href="https://etherpad-jitsi.apps.cloud.example.com/p/Podium" target="_tab" rel="noreferrer noopener">General Podium Pad</a></li> <li><a href="https://etherpad-jitsi.apps.cloud.example.com/p/Draft" target="_tab" rel="noreferrer noopener">Draft Pad</a></li> <li><a href="https://etherpad-jitsi.apps.cloud.example.com/p/ToDo" target="_tab" rel="noreferrer noopener">ToDo Pad</a></li></ul>',
                     columns: 1, rows: 2,
                     x: 3, y: 1
                 },
                 {
                     type: 'embed.markup',
                     title: 'Knowledge Base',
-                    content: '<center><a href="http://dokuwiki-jitsi.apps.cloud.lunetix.org/" target="_tab" rel="noreferrer noopener"><img src="https://www.dokuwiki.org/_media/wiki:dokuwiki-128.png" alt="Podium Wiki" width="300" height="200"></a> <a href="https://openpracticelibrary.com/" target="_tab" rel="noreferrer noopener"><img src="https://d33wubrfki0l68.cloudfront.net/337d8258554af2343978fdbcd7e854298ab8062c/e7414/images/logo.svg" alt="Open Practice Library" width="300" height="200"></a></center>',
+                    content: '<center><a href="http://dokuwiki-jitsi.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener"><img src="https://www.dokuwiki.org/_media/wiki:dokuwiki-128.png" alt="Podium Wiki" width="300" height="200"></a> <a href="https://openpracticelibrary.com/" target="_tab" rel="noreferrer noopener"><img src="https://d33wubrfki0l68.cloudfront.net/337d8258554af2343978fdbcd7e854298ab8062c/e7414/images/logo.svg" alt="Open Practice Library" width="300" height="200"></a></center>',
                     columns: 2, rows: 1,
                     x: 1, y: 2
                 },
                 {
                     type: 'embed.markup',
                     title: 'Chat',
-                    content: '<ul><li><a href="https://chat-mattermost.apps.cloud.lunetix.org" target="_blank" rel="noreferrer noopener">New Chat</a></li></ul>',
+                    content: '<ul><li><a href="https://chat-mattermost.apps.cloud.example.com" target="_blank" rel="noreferrer noopener">New Chat</a></li></ul>',
                     columns: 1, rows: 1,
                     x: 0, y: 2
                 }
