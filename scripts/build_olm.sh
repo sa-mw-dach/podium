ea5e9c883a230209d18adcee093fc225a4cbe816
Keith Tenzer <keith.tenzer@gmail.com>
2020-04-28 18:27:03 +0000

    incremented version of csv

--- scripts/build_olm.sh
+++ scripts/build_olm.sh
@@ -2,27 +2,27 @@
 
 cd ../podium-operator
 echo "Building Operator..."
-sudo operator-sdk build quay.io/ktenzer/podium-operator:latest
+sudo operator-sdk build quay.io/ktenzer/podium-operator:1.0.1
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Pushing operator to repository..."
-sudo docker push quay.io/ktenzer/podium-operator:latest
+sudo docker push quay.io/ktenzer/podium-operator:1.0.1
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Building OLM catalog bundle..."
-sudo operator-sdk bundle create quay.io/ktenzer/podium-operator-catalog:latest --channels alpha --package podium-operator-catalog --directory deploy/olm-catalog/podium-operator/1.0.0
+sudo operator-sdk bundle create quay.io/ktenzer/podium-operator-catalog:1.0.1 --channels alpha --package podium-operator-catalog --directory deploy/olm-catalog/podium-operator/1.0.1
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Pushing bundle to repository..."
-sudo docker push quay.io/ktenzer/podium-operator-catalog:latest
+sudo docker push quay.io/ktenzer/podium-operator-catalog:1.0.1
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Building OPM Catalog Index..."
-sudo opm index add -c docker --bundles quay.io/ktenzer/podium-operator-catalog:latest --tag quay.io/ktenzer/podium-operator-index:latest
+sudo opm index add -c docker --bundles quay.io/ktenzer/podium-operator-catalog:1.0.1 --tag quay.io/ktenzer/podium-operator-index:1.0.1
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Pushing Catalog Index to registry..."
-sudo docker push quay.io/ktenzer/podium-operator-index:latest
+sudo docker push quay.io/ktenzer/podium-operator-index:1.0.1
 if [ $? != 0 ]; then exit 1; fi
 
 echo "OLM Operator build completed successfully"
