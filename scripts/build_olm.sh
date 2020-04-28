6e5fe1704f93f63551d8d9be1a260aacec97bcf6
Keith Tenzer <keith.tenzer@gmail.com>
2020-04-28 16:06:30 +0000

    updated image

--- scripts/build_olm.sh
+++ scripts/build_olm.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-echo "Building OLM catalog bundle..."
 cd ../podium-operator
-sudo operator-sdk bundle create quay.io/ktenzer/podium-operator-catalog:latest --channels beta --package podium-operator-catalog --directory deploy/olm-catalog/podium-operator/manifests
+echo "Building Operator..."
+sudo operator-sdk build quay.io/ktenzer/podium-operator:latest
+if [ $? != 0 ]; then exit 1; fi
+
+echo "Pushing operator to repository..."
+sudo docker push quay.io/ktenzer/podium-operator:latest
+if [ $? != 0 ]; then exit 1; fi
+
+echo "Building OLM catalog bundle..."
+sudo operator-sdk bundle create quay.io/ktenzer/podium-operator-catalog:latest --channels alpha --package podium-operator-catalog --directory deploy/olm-catalog/podium-operator/1.0.0
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Pushing bundle to repository..."
