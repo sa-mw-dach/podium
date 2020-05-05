439025be0aa84e8955e6552a0e60e87aaa319685
Keith Tenzer <ktenzer@redhat.com>
2020-05-05 12:54:03 +0000

    added operator version 1.0.4, created quay repository for podium and added images there instead of under ktenzer account

--- scripts/build_olm.sh
+++ scripts/build_olm.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
-LAST_VERSION="1.0.2"
-VERSION="1.0.3"
+VERSION="1.0.4"
 
 cd ../podium-operator
 echo "Building Operator..."
-sudo operator-sdk build quay.io/ktenzer/podium-operator:$VERSION
+sudo operator-sdk build quay.io/podium/podium-operator:$VERSION
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Pushing operator to repository..."
-sudo docker push quay.io/ktenzer/podium-operator:$VERSION
+sudo docker push quay.io/podium/podium-operator:$VERSION
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Building OLM catalog bundle..."
-sudo operator-sdk bundle create quay.io/ktenzer/podium-operator-catalog:$VERSION --channels alpha --package podium-operator-catalog --directory deploy/olm-catalog/podium-operator/$VERSION
+sudo operator-sdk bundle create quay.io/podium/podium-operator-catalog:$VERSION --channels alpha --package podium-operator-catalog --directory deploy/olm-catalog/podium-operator/$VERSION
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Pushing bundle to repository..."
-sudo docker push quay.io/ktenzer/podium-operator-catalog:$VERSION
+sudo docker push quay.io/podium/podium-operator-catalog:$VERSION
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Building OPM Catalog Index..."
-#sudo opm index add -c docker --bundles quay.io/ktenzer/podium-operator-catalog:$VERSION --tag quay.io/ktenzer/podium-operator-index:$VERSION
-sudo opm index add -c docker --bundles quay.io/ktenzer/podium-operator-catalog:$LAST_VERSION --bundles quay.io/ktenzer/podium-operator-catalog:$VERSION --tag quay.io/ktenzer/podium-operator-index:$VERSION
+sudo opm index add -c docker --bundles quay.io/podium/podium-operator-catalog:1.0.2 --bundles quay.io/podium/podium-operator-catalog:1.0.3 --bundles quay.io/podium/podium-operator-catalog:$VERSION --tag quay.io/podium/podium-operator-index:$VERSION
 if [ $? != 0 ]; then exit 1; fi
 
 echo "Pushing Catalog Index to registry..."
-sudo docker push quay.io/ktenzer/podium-operator-index:$VERSION
+sudo docker push quay.io/podium/podium-operator-index:$VERSION
 if [ $? != 0 ]; then exit 1; fi
 
 echo "OLM Operator build completed successfully"
