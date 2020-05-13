5b72a5dc1ce4eee80f3e0ad34fbc6c835c31fb6d
Sebastian Hetze <shetze@redhat.com>
2020-05-13 22:09:42 +0200

    update default config.js

--- mozaik/dashboard/config.js
+++ mozaik/dashboard/config.js
@@ -30,7 +30,6 @@ var config = {
 
     dashboards: [
 
-        // first dashboard
         {
             // 4 x 3 dashboard
             columns: 4,
@@ -56,7 +55,7 @@ var config = {
                 {
                     type: 'embed.markup',
                     title: 'Conference Center',
-                    content: '<center><a href="https://meet-jitsi.apps.cloud.example.com/Plenum" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2013/02/20/01/04/meeting-83519__340.jpg" alt="Main Podium" width="300" height="200"></a>  <a href="https://meet-jitsi.apps.cloud.example.com/RoomA" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2015/07/02/09/52/interior-design-828545__340.jpg" alt="Meeting Room" width="300" height="200"></a>  <a href="https://meet-jitsi.apps.cloud.example.com/Lobby" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2019/11/29/08/34/space-4660847__340.jpg" alt="Lobby" width="300" height="200"></a></center>',
+                    content: '<center><a href="https://meet-podium.apps.cloud.example.com/Plenum" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2013/02/20/01/04/meeting-83519__340.jpg" alt="Main Plenum" width="300" height="200"></a>  <a href="https://meet-podium.apps.cloud.example.com/Roundtable" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2015/07/02/09/52/interior-design-828545__340.jpg" alt="Roundtable" width="300" height="200"></a>  <a href="https://meet-podium.apps.cloud.example.com/Lobby" target="_blank" rel="noreferrer noopener"><img src="https://cdn.pixabay.com/photo/2019/11/29/08/34/space-4660847__340.jpg" alt="Lobby" width="300" height="200"></a></center>',
                     columns: 2, rows: 1,
                     x: 1, y: 1
                 },
@@ -67,29 +66,29 @@ var config = {
                 },
                 {
                     type: 'embed.markup',
-                    title: 'Kanban Board',
-                    content: '<center><a href="https://wekan-jitsi.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener"><img src="https://wekan.github.io/wekan-logo.svg" alt="Podium Kanban" width="300" height="200"></a></center>',
+                    title: 'Knowledge Base',
+                    content: '<center><a href="https://dokuwiki-podium.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener"><img src="https://www.dokuwiki.org/_media/wiki:dokuwiki-128.png" alt="Plenum Wiki" width="200" height="120"></a> <a href="https://openpracticelibrary.com/" target="_tab" rel="noreferrer noopener"><img src="https://d33wubrfki0l68.cloudfront.net/337d8258554af2343978fdbcd7e854298ab8062c/e7414/images/logo.svg" alt="Open Practice Library" width="200" height="120"></a></center>',
                     columns: 1, rows: 1,
                     x: 0, y: 1
                 },
                 {
                     type: 'embed.markup',
-                    title: 'Sketch Boards',
-                    content: '<ul><li><a href="https://etherpad-jitsi.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener">New Pad</a></li> <li><a href="https://etherpad-jitsi.apps.cloud.example.com/p/Podium" target="_tab" rel="noreferrer noopener">General Podium Pad</a></li> <li><a href="https://etherpad-jitsi.apps.cloud.example.com/p/Draft" target="_tab" rel="noreferrer noopener">Draft Pad</a></li> <li><a href="https://etherpad-jitsi.apps.cloud.example.com/p/ToDo" target="_tab" rel="noreferrer noopener">ToDo Pad</a></li></ul>',
+                    title: 'Collaboration Boards',
+                    content: '<a href="https://etherpad-podium.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener"><img src="https://raw.githubusercontent.com/sa-mw-dach/podium/master/docs/images/etherpad-300x600.png" alt="Etherpad" width="300" height="150"></a>  <ul> <li><a href="https://etherpad-podium.apps.cloud.example.com/p/Plenum" target="_tab" rel="noreferrer noopener">Plenum Agenda and Notes</a></li> <li><a href="https://etherpad-podium.apps.cloud.example.com/p/Roundtable" target="_tab" rel="noreferrer noopener">Roundtable Agenda and Notes</a></li> <li><a href="https://etherpad-podium.apps.cloud.example.com/p/Lobby" target="_tab" rel="noreferrer noopener">Lobby Agenda and Notes</a></li> <li><a href="https://etherpad-podium.apps.cloud.example.com/p/ToDo" target="_tab" rel="noreferrer noopener">ToDo Pad</a></li></ul><p><a href="https://etherdraw-podium.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener"><img src="https://raw.githubusercontent.com/sa-mw-dach/podium/master/docs/images/etherdraw-300x600.png" alt="Etherdraw" width="300" height="150"></a>  <ul> <li><a href="https://etherdraw-podium.apps.cloud.example.com/d/Plenum" target="_tab" rel="noreferrer noopener">Plenum Whiteboard</a></li> <li><a href="https://etherdraw-podium.apps.cloud.example.com/d/Roundtable" target="_tab" rel="noreferrer noopener">Roundtable Whiteboard</a></li> <li><a href="https://etherdraw-podium.apps.cloud.example.com/d/Lobby" target="_tab" rel="noreferrer noopener">Lobby Whiteboard</a></li> <li><a href="https://etherdraw-podium.apps.cloud.example.com/d/Draft" target="_tab" rel="noreferrer noopener">Draft Whiteboard</a></li></ul>',
                     columns: 1, rows: 2,
                     x: 3, y: 1
                 },
                 {
                     type: 'embed.markup',
-                    title: 'Knowledge Base',
-                    content: '<center><a href="http://dokuwiki-jitsi.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener"><img src="https://www.dokuwiki.org/_media/wiki:dokuwiki-128.png" alt="Podium Wiki" width="300" height="200"></a> <a href="https://openpracticelibrary.com/" target="_tab" rel="noreferrer noopener"><img src="https://d33wubrfki0l68.cloudfront.net/337d8258554af2343978fdbcd7e854298ab8062c/e7414/images/logo.svg" alt="Open Practice Library" width="300" height="200"></a></center>',
+                    title: 'Productivity Tools',
+                    content: '<center><a href="https://drawio-podium.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener"><img src="https://cdn.worldvectorlogo.com/logos/draw-io.svg" alt="Podium Draw.io" width="300" height="200"></a><a href="https://wekan-podium.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener"><img src="https://wekan.github.io/wekan-logo.svg" alt="Podium Kanban" width="300" height="200"></a><a href="https://mindmaps-podium.apps.cloud.example.com/" target="_tab" rel="noreferrer noopener"><img src="https://raw.githubusercontent.com/sa-mw-dach/podium/master/docs/images/mindmap.png" alt="Podium Mindmaps" width="300" height="200"></center>',
                     columns: 2, rows: 1,
                     x: 1, y: 2
                 },
                 {
                     type: 'embed.markup',
                     title: 'Chat',
-                    content: '<ul><li><a href="https://chat-mattermost.apps.cloud.example.com" target="_blank" rel="noreferrer noopener">New Chat</a></li></ul>',
+                    content: '<center><a href="https://chat-mattermost.apps.cloud.example.com" target="_blank" rel="noreferrer noopener"><img src="https://cdn.freebiesupply.com/logos/large/2x/mattermost-logo-png-transparent.png" alt="Mattermost Chat" width="300" height="200"></a></center>',
                     columns: 1, rows: 1,
                     x: 0, y: 2
                 }
